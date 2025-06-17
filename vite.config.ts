034ef4c0c5b2e393c304930f1b511f393c83e1df
website-krysta <website.krysta@gmail.com>
2025-06-17 17:45:09 +0530

    fixed issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/" : "/",
+  base: "/",
+  // base: mode === "production" ? "/evolv-brandblueprint/" : "/",
   server: {
     host: "::",
     port: 8080,
