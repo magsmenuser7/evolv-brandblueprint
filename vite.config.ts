f39869fa390ef427949c919abc6657d95e6a2ea6
website-krysta <website.krysta@gmail.com>
2025-06-17 22:54:44 +0530

    update code

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
-  // base: mode === "production" ? "/evolv-brandblueprint/" : "/",
+  // base: "/",
+  base: mode === "production" ? "/evolv-brandblueprint/" : "/",
   server: {
     host: "::",
     port: 8080,
