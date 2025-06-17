22a251246420fdfa72db07ff5cb922ed0fb94fd6
website-krysta <website.krysta@gmail.com>
2025-06-17 22:59:00 +0530

    chnage domain name

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: "/",
-  base: mode === "production" ? "/evolv-brandblueprint/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
